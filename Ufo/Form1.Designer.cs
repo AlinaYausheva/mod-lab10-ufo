9cf135c9a9aa5734e74f408aef8e207f247b2549
AlinaYausheva <43522405+AlinaYausheva@users.noreply.github.com>
2023-05-31 16:14:54 +0300

    Add files via upload

---
+++ Ufo/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace Ufo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.drawing_button = new System.Windows.Forms.Button();
+            this.graphics_button = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // drawing_button
+            // 
+            this.drawing_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.drawing_button.Location = new System.Drawing.Point(79, 545);
+            this.drawing_button.Name = "drawing_button";
+            this.drawing_button.Size = new System.Drawing.Size(294, 65);
+            this.drawing_button.TabIndex = 1;
+            this.drawing_button.Text = "Смоделировать движение точки";
+            this.drawing_button.UseVisualStyleBackColor = true;
+            this.drawing_button.Click += new System.EventHandler(this.drawing_button_Click);
+            // 
+            // graphics_button
+            // 
+            this.graphics_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.graphics_button.Location = new System.Drawing.Point(649, 545);
+            this.graphics_button.Name = "graphics_button";
+            this.graphics_button.Size = new System.Drawing.Size(294, 65);
+            this.graphics_button.TabIndex = 2;
+            this.graphics_button.Text = "Построить график";
+            this.graphics_button.UseVisualStyleBackColor = true;
+            this.graphics_button.Click += new System.EventHandler(this.graphics_button_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // chart1
+            // 
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Name = "chart1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series1.YValuesPerPoint = 2;
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(1003, 527);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.chart1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1003, 527);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1027, 622);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.graphics_button);
+            this.Controls.Add(this.drawing_button);
+            this.Name = "Form1";
+            this.Text = "Моделирование движение по прямой";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button drawing_button;
+        private System.Windows.Forms.Button graphics_button;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Ufo/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Ufo
+{
+    public partial class Form1 : Form
+    {
+        Point start;
+        Point end;
+        PointF current;
+        double angle;
+        int accuracy = 5;
+        float step;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            chart1.Visible = false;
+            panel1.Visible = false;
+        }
+
+        double Factorial(int n)
+        {
+            if (n == 0)
+                return 1;
+            return n * Factorial(n - 1);
+        }
+        double Sin(double x, int n)
+        {
+            double ans = 0;
+            for (int i = 1; i <= n; i++)
+                ans += Math.Pow(-1, i - 1) * Math.Pow(x, 2 * i - 1) / Factorial(2 * i - 1);
+
+            return ans;
+        }
+
+        double Cos(double x, int n)
+        {
+            double ans = 0;
+            for (int i = 1; i <= n; i++)
+                ans += Math.Pow(-1, i - 1) * Math.Pow(x, 2 * i - 2) / Factorial(2 * i - 2);
+
+            return ans;
+        }
+
+        double Arctg(double x, int n)
+        {
+            double ans = 0;
+            for (int i = 1; i <= n; i++)
+                ans += Math.Pow(-1, i - 1) * Math.Pow(x, (2 * i - 1)) / (2 * i - 1);
+
+            return ans;
+        }
+
+        private double Count_distance(int n)
+        {
+            double distance = 0;
+            current = start;
+
+            if (Math.Abs(end.Y - start.Y) <= Math.Abs(end.X - start.X))
+            {
+                angle = Arctg((double)Math.Abs(end.Y - start.Y) / Math.Abs(end.X - start.X), n);
+            }
+            else
+            {
+                angle = Math.PI / 2 - Arctg((double)Math.Abs(end.X - start.X) / Math.Abs(end.Y - start.Y), n);
+            }
+            while ((end.X > current.X) && (end.Y > current.Y))
+            {
+                distance = (double)Math.Sqrt(Math.Pow(current.X - end.X, 2) + Math.Pow(current.Y - end.Y, 2));
+                current.X += (float)(step * Cos(angle, n));
+                current.Y += (float)(step * Sin(angle, n));
+            }
+            return distance;
+        }
+
+        private void drawing_button_Click(object sender, EventArgs e)
+        {
+            start = new Point(50, 50);
+            end = new Point(600, 250);
+            step = 5;
+            panel1.Visible = true;
+            chart1.Visible = false;
+            current = start;
+
+            if (Math.Abs(end.Y - start.Y) <= Math.Abs(end.X - start.X))
+            {
+                angle = Arctg((double)Math.Abs(end.Y - start.Y) / Math.Abs(end.X - start.X), accuracy);
+            }
+            else
+            {
+                angle = Math.PI / 2 - Arctg((double)Math.Abs(end.X - start.X) / Math.Abs(end.Y - start.Y), accuracy);
+            }
+            timer1.Start();
+        }
+
+        private void graphics_button_Click(object sender, EventArgs e)
+        {
+            start = new Point(10, 10);
+            end = new Point(350, 380);
+            step = 1;
+            chart1.Visible = true;
+            panel1.Visible = true;
+
+            chart1.Series.Clear();
+            chart1.Series.Add("График зависимости количества членов ряда от радиуса зоны попадания");
+            chart1.ChartAreas.Add("График зависимости количества членов ряда от радиуса зоны попадания");
+            chart1.Series[0].ChartType = SeriesChartType.Spline;
+            chart1.ChartAreas[0].AxisX.Title = "Радиус зоны попадания";
+            chart1.ChartAreas[0].AxisY.Title = "Количество членов ряда";
+            chart1.ChartAreas[0].AxisX.Minimum = 0;
+
+            for (int i = 2; i <= 50; i ++)
+                chart1.Series[0].Points.AddXY(Count_distance(i), i);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if ((end.X <= current.X)&& (end.Y <= current.Y)){
+                timer1.Stop();
+            }
+            else
+            {
+                current.X += (float)(step * Cos(angle, accuracy));
+                current.Y += (float)(step * Sin(angle, accuracy));
+                panel1.Invalidate();
+            }        
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            GraphicsState gs;
+
+            Pen pen = new Pen(Color.Black, 8);
+            g.DrawEllipse(pen, start.X, start.Y, 2, 2);
+            g.DrawEllipse(pen, end.X, end.Y, 2, 2);
+            SolidBrush brush = new SolidBrush(Color.Red);
+            g.FillEllipse(brush, current.X - step, current.Y - step, 2 * step, 2 * step);
+
+            gs = g.Save();
+            g.Restore(gs);
+        }
+    }
+}
